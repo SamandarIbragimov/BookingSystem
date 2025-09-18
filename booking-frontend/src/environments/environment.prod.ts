051657dd7b7e94c713aa2d944354fd188a8b5b73
Samandar Ibragimov <samandar0406@gmail.com>
2025-09-19 00:01:12 +0200

    front added

---
+++ booking-frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://localhost:5059' // to be set when deployed
+};
---
+++ booking-frontend/src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:5059' // adjust to your backend local port
+};
