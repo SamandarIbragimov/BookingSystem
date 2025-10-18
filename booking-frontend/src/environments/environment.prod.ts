e08deef505e45364bb9128a5f3776cd5e9f7ba6c
Samandar Ibragimov <samandar0406@gmail.com>
2025-10-18 16:45:15 +0200

    Angular app is removed from repo

--- booking-frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://bookingsystem-ci6c.onrender.com' // to be set when deployed
-};
--- booking-frontend/src/environments/environments.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'https://bookingsystem-ci6c.onrender.com' // adjust to your backend local port
-};
