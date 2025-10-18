2ef913e9e35fee45d9485c8de7b239b2cd659027
Samandar Ibragimov <samandar0406@gmail.com>
2025-10-18 15:54:07 +0200

    server url added

--- booking-frontend/src/environments/environment.prod.ts
+++ booking-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '' // to be set when deployed
+  apiBaseUrl: 'https://bookingsystem-ci6c.onrender.com' // to be set when deployed
 };
